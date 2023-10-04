49f25b2f078890031ef703778d082e335718d04a
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-10-04 22:36:56 +0300

    Added info about task

--- gamefiles/Main.java
+++ gamefiles/Main.java
@@ -2,20 +2,27 @@ package gamefiles;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Menu.printUserMenu();
         NumberGame numberGame = new NumberGame();
-        try (Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) 
+        {
             numberGame.start(scanner.nextInt(), scanner.nextInt());
             scanner.nextLine();
-            while (!numberGame.getGameStatus().equals(GameStatus.FINISH)){
+            while (!numberGame.getGameStatus().equals(GameStatus.FINISH))
+            {
                 String value = scanner.nextLine();
                 Answer answer = numberGame.inputValue(value);
-              if (answer!=null){
-                  System.out.println("answer = " + answer);
-              }
+                if (answer!=null)
+                {
+                    System.out.println("answer = " + answer);
+                }
                 System.out.println(numberGame.getMoveHistory());
             }
         }
     }
 }
+
